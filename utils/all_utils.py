367035bd6452ff5808fb3fe9047998fc5f9aaa36
siddhartha borgohain <80624931+siddharthaborgohain@users.noreply.github.com>
2022-07-16 05:18:32 +0530

    first commit

---
+++ utils/all_utils.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import joblib # FOR SAVING MY MODEL AS A BINARY FILE
+from matplotlib.colors import ListedColormap
+
+
+
+def prepare_data(df):
+  X = df.drop("y", axis=1)
+
+  y = df["y"]
+
+  return X, y
